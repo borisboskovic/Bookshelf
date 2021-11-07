8972f479de1f63c20e2ea760f20c76b2b340bcb5
Boris Boskovic <boris.boskovic@lanaco.com>
2021-11-07 23:03:33 +0100

    Added peek password to input fields

--- bookshelf-client/src/components/Ui/Validation/InputField.vue
+++ bookshelf-client/src/components/Ui/Validation/InputField.vue
@@ -1,12 +1,25 @@
 <template>
 	<label :for="name" class="input-label">{{ label }}</label>
-	<Field :name="name" :type="type" class="input-component" />
+	<div class="field-container">
+		<Field :name="name" :type="fieldType" :class="fieldClasses" />
+		<i
+			v-if="type === 'password' && peekPassword"
+			class="fa fa-eye"
+			@click="togglePeekPassword"
+		/>
+		<i
+			v-if="type === 'password' && !peekPassword"
+			class="fa fa-eye-slash"
+			@click="togglePeekPassword"
+		/>
+	</div>
 	<div class="error-container">
 		<ErrorMessage :name="name" class="error-message" />
 	</div>
 </template>
 
 <script>
+	import { ref, computed } from "vue";
 	import { Field, ErrorMessage } from "vee-validate";
 
 	export default {
@@ -18,6 +31,34 @@
 			label: String,
 			name: String,
 			type: String,
+		},
+		setup: (props) => {
+			const peekPassword = ref(false);
+
+			const togglePeekPassword = () => {
+				peekPassword.value = !peekPassword.value;
+			};
+
+			const fieldType = computed(() => {
+				if (props.type !== "password") {
+					return props.type;
+				} else if (!peekPassword.value) {
+					return "password";
+				} else {
+					return "text";
+				}
+			});
+
+			const fieldClasses = `input-component ${
+				fieldType.value === "password" && "field-with-icon"
+			}`;
+
+			return {
+				peekPassword,
+				togglePeekPassword,
+				fieldType,
+				fieldClasses,
+			};
 		},
 	};
 </script>
